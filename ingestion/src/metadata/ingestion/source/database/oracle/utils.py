9276fbae02335a488e8e71014893d8a190cebc37
varun-lakhyani <lakhiyanivarun14@gmail.com>
2026-03-11 22:37:48 +0530

    changed default behaviour

--- ingestion/src/metadata/ingestion/source/database/oracle/utils.py
+++ ingestion/src/metadata/ingestion/source/database/oracle/utils.py
@@ -38,11 +38,11 @@ from metadata.utils.sqlalchemy_utils import (
 
 
 def get_table_prefix_from_connection(service_connection) -> str:
-    return "DBA" if getattr(service_connection, "useDBATable", False) else "ALL"
+    return "DBA" if getattr(service_connection, "useDBATable", True) else "ALL"
 
 
 def _get_table_prefix(self) -> str:
-    return getattr(self, "table_prefix", "ALL")
+    return getattr(self, "table_prefix", "DBA")
 
 
 @reflection.cache
